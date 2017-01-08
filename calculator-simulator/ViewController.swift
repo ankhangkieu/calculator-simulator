5f2c5f11a006a84d63a55605f06135212aec5770
ankhangkieu <ankhangkieu@gmail.com>
2017-01-08 00:42:29 -0800

    add all calculator's interface and fix old ones

--- calculator-simulator/ViewController.swift
+++ calculator-simulator/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var resultLbl: UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
